566eb6b87dbe3b30d99ec087a5aa7d340cf25bd9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-28 14:03:17 -0400

    Provides more option Improves generation

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 /* global gpf, XMLWorkbook */
 
 const tags = {}
-'div,span,form,label,button,select,option'
+'h1,p,div,span,form,label,button,select,option'
   .split(',').forEach(tag => { tags[tag] = gpf.web.createTagFunction(tag) })
 
-const weekDays = [1, 2, 3, 4, 5, 6] // Ignore sunday
 const DAY = 24 * 60 * 60 * 1000
 
 const FORMAT_NORMAL = 0
@@ -34,9 +33,9 @@ function clearTime (date) {
   date.setMilliseconds(0)
 }
 
-function generateFor (year) {
+function generateFor (year, weekDays, breakoutType) {
   console.log('Generating calendar for year ' + year)
-  const workbook = new XMLWorkbook()
+  const workbook = new XMLWorkbook(year, weekDays)
   const janFirst = new Date(year, 0, 1, 0, 0, 0, 0)
   let day = janFirst
   // Search for the first day of the week
@@ -45,11 +44,17 @@ function generateFor (year) {
   }
   console.log('First day of the week for this year: ' + dateToUsrFmt(day))
   // Breakout dates
-  const breakoutDates = [new Date(year, 0, 15)]
-  for (var month = 1; month < 13; ++month) {
-    breakoutDates.push(new Date(year, month, 0), new Date(year, month, 15))
+  const breakoutDates = []
+  if (breakoutType === 2) {
+      breakoutDates.push(new Date(year, 0, 15))
   }
-  breakoutDates.push(new Date(year, 11, 31));
+  for (var month = 1; month < 13; ++month) {
+    breakoutDates.push(new Date(year, month, 0))
+    if (breakoutType === 2) {
+        breakoutDates.push(new Date(year, month, 15))
+    }
+  }
+  breakoutDates.push(new Date(year, 11, 31))
   breakoutDates.forEach(clearTime)
   console.log('Breakout dates:')
   breakoutDates.forEach(date => console.log(`\t${dateToUsrFmt(date)}`))
@@ -58,13 +63,13 @@ function generateFor (year) {
   const formats = []
   let breakout = false
   let lastBreakoutDate
-  let loops = 100
+  let maxNumberOfLoops = 100 // Security against infinite loops :-)
   let firstDayOfWeek = day
-  while (day.getFullYear() !== year + 1 && --loops) {
+  while (day.getFullYear() !== year + 1 && --maxNumberOfLoops) {
     dates.length = 0
     formats.length = 0
     weekDays.forEach(() => {
-      dates.push(`%c${dateToUsrFmt(day)}`)
+      dates.push(day)
       if (day.getFullYear() !== year) {
         formats.push(FORMAT_OUT)
       } else if (lastBreakoutDate && day <= lastBreakoutDate) {
@@ -79,11 +84,14 @@ function generateFor (year) {
       clearTime(day)
     })
     if (formats.includes(FORMAT_NORMAL)) {
-      console.log.apply(console, [dates.join(' ')].concat(formats.map(format => FORMAT_CONSOLE[format])));
+      console.log.apply(console, [dates.map(date => `%c${dateToUsrFmt(date)}`).join(' ')]
+        .concat(formats.map(format => FORMAT_CONSOLE[format])))
+      workbook.renderWeek(dates, formats)
     }
     if (breakout) {
-      console.log(`%c--- BREAKOUT OF ${dateToUsrFmt(breakoutDates[0])} ---`, 'color: red')
       lastBreakoutDate = breakoutDates.shift()
+      console.log(`%c--- BREAKOUT OF ${dateToUsrFmt(lastBreakoutDate)} ---`, 'color: red')
+      workbook.renderBreakout(lastBreakoutDate)
       breakout = false
     } else {
       firstDayOfWeek = new Date(firstDayOfWeek.getTime() + 7 * DAY)
@@ -94,23 +102,48 @@ function generateFor (year) {
   return workbook.toString()
 }
 
+function getSelectedValue (id) {
+  const select = document.getElementById(id)
+  return select.options[select.selectedIndex].value
+}
+
 window.addEventListener('load', () => {
   const currentYear = (new Date()).getFullYear()
-  tags.form([
-    tags.div({ className: 'form-group' }, [
-      tags.label({ for: 'year' }, 'Select year'),
-      tags.select({ className: 'form-control', id: 'year' }, [
-        tags.option({ value: currentYear }, currentYear),
-        tags.option({ value: currentYear + 1 }, currentYear + 1)
-      ])
-    ]),
-    tags.button({ className: 'btn btn-primary', id: 'generate' }, 'Generate')
+
+  tags.div({ className: 'jumbotron' }, [
+    tags.h1(document.title),
+    tags.p(tags.form([
+      tags.div({ className: 'form-group' }, [
+        tags.label({ for: 'year' }, 'Select year'),
+        tags.select({ className: 'form-control', id: 'year' }, [
+          tags.option({ value: currentYear }, currentYear),
+          tags.option({ value: currentYear + 1 }, currentYear + 1)
+        ])
+      ]),
+      tags.div({ className: 'form-group' }, [
+        tags.label({ for: 'week' }, 'Select week type'),
+        tags.select({ className: 'form-control', id: 'week' }, [
+          tags.option({ value: '1,2,3,4,5,6' }, 'Mon, Tue, Wed, Thu, Fri, Sat'),
+          tags.option({ value: '1,2,3,4,5' }, 'Mon, Tue, Wed, Thu, Fri')
+        ])
+      ]),
+      tags.div({ className: 'form-group' }, [
+        tags.label({ for: 'breakouts' }, 'Select breakout type'),
+        tags.select({ className: 'form-control', id: 'breakouts' }, [
+          tags.option({ value: '2' }, 'Twice per month (15 / end of month)'),
+          tags.option({ value: '1' }, 'Every month')
+        ])
+      ]),
+      tags.button({ className: 'btn btn-primary', id: 'generate' }, 'Generate')
+    ]))
   ]).appendTo(document.body)
+
   tags.div({ id: 'timesheet' }).appendTo(document.body)
   document.getElementById('generate').addEventListener('click', () => {
     window.event.preventDefault()
-    const yearSelect = document.getElementById('year')
-    const selectedYear = parseInt(yearSelect.options[yearSelect.selectedIndex].value, 10)
-    /*location.href =*/ generateFor(selectedYear)
+    const selectedYear = parseInt(getSelectedValue('year'), 10)
+    const selectedWeek = getSelectedValue('week').split(',').map(day => parseInt(day, 10))
+    const breakoutType = parseInt(getSelectedValue('breakouts'), 10)
+    location.href = generateFor(selectedYear, selectedWeek, breakoutType)
   })
 })
